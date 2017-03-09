90dd8e0bba66fd66d288d625ce85ff21ff9b59d9
root <root@localhost.localdomain>
2017-03-09 02:48:13 -0500

    change

--- modules/ps_test.py
+++ modules/ps_test.py
@@ -3,7 +3,9 @@ import subprocess
 def run(**args):
 
 	print "[*] In ps_test module."
-	data = subprocess.call('ps -ef',shell=True)
+	fd_popen = subprocess.call('ps -ef',stdout=subprocess.PIPE).stdout
+	data = fd_popen.read().strip()
 	
+	print data
 	return str(data)
 
