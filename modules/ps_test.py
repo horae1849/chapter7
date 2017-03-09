2d7d206f34578811f3ae1ef2f60aa8060185ef45
root <root@localhost.localdomain>
2017-03-09 02:57:30 -0500

    change

--- modules/ps_test.py
+++ modules/ps_test.py
@@ -3,7 +3,8 @@ import subprocess
 def run(**args):
 
 	print "[*] In ps_test module."
-	fd_popen = subprocess.call('ps -ef',stdout=subprocess.PIPE).stdout
+	cmd = ["ps","-ef"]
+	fd_popen = subprocess.call(cmd,stdout=subprocess.PIPE).stdout
 	data = fd_popen.read().strip()
 	
 	print data
